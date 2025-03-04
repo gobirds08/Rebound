9352c01cf578a472bbc708cb66d8109ff0a59fea
Brendan Kenney <brendankenney3@gmail.com>
2025-03-03 20:39:11 -0500

    New State plus handlers

--- Rebound/src/Game.cpp
+++ Rebound/src/Game.cpp
@@ -2,6 +2,16 @@
 
 Game::Game() {
 	m_window.create(sf::VideoMode({ HEIGHT, WIDTH }), "Rebound", sf::Style::Titlebar | sf::Style::Close);
+	//m_target_circle = sf::CircleShape(50);
+    m_game_state = GameState::MainMenu;
+}
+
+GameState Game::getGameState() {
+    return m_game_state;
+}
+
+void Game::setGameState(GameState gameState) {
+    m_game_state = gameState;
 }
 
 void Game::run() {
@@ -9,6 +19,9 @@ void Game::run() {
         handleEvents();
 
         m_window.clear();
+
+        // TODO: Draw Shapes In Game 
+
         m_window.display();
     }
 }
@@ -18,10 +31,27 @@ void Game::handleEvents() {
         if (event->is<sf::Event::Closed>()) {
             m_window.close();
         } 
-        else if (const auto* keyPressed = event->getIf<sf::Event::KeyPressed>()) {
-            handleKeyPress(keyPressed->scancode);
+
+        if (m_game_state == GameState::InGame) {
+            handleInGameEvents(event.value());
+        }
+        else if (m_game_state == GameState::MainMenu) {
+            handleMainMenuEvents(event.value());
         }
     }
+}
+
+
+void Game::handleInGameEvents(const sf::Event& event) {
+    if (const auto* keyPressed = event.getIf<sf::Event::KeyPressed>()) {
+        handleKeyPress(keyPressed->scancode);
+    } 
+
+    // TODO: Handle Mouse Movement For Player
+}
+
+void Game::handleMainMenuEvents(const sf::Event& event) {
+    // TODO: Implementation
 }
 
 void Game::handleKeyPress(sf::Keyboard::Scancode code) {
--- Rebound/src/headers/Game.h
+++ Rebound/src/headers/Game.h
@@ -4,18 +4,30 @@
 
 constexpr int HEIGHT = 800;
 constexpr int WIDTH = 500;
+enum GameState { MainMenu, InGame };
 
 class Game {
+public:
+	/*enum GameState { MainMenu, InGame };*/
+
 public:
 	Game();
 
 	void run();
+	GameState getGameState();
+	void setGameState(GameState gameState);
+
 
 private:
 	sf::RenderWindow m_window;
+	sf::CircleShape m_target_circle;
+	sf::RectangleShape m_player_rect;
+	GameState m_game_state;
 
 private:
 	void handleEvents();
 	void handleKeyPress(sf::Keyboard::Scancode code);
+	void handleInGameEvents(const sf::Event& event);
+	void handleMainMenuEvents(const sf::Event& event);
 };
 
